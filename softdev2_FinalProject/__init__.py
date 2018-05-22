697a72011d5d861fbc70ad04b5e3c9d88bfddea0
Max Korsun <mkorsun@stuy.edu>
2018-05-22 14:36:04 -0400

    restructed for deployment testing

---
+++ softdev2_FinalProject/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.debug = False
+    app.run()
