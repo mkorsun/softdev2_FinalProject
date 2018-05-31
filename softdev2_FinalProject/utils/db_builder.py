0f2cb50db1a68877635b3845937409fca7b0f0fa
clei1 <clei1@stuy.edu>
2018-05-31 10:10:57 -0400

    db functions

--- softdev2_FinalProject/utils/db_builder.py
+++ softdev2_FinalProject/utils/db_builder.py
@@ -8,8 +8,9 @@ c = db.cursor()  #facilitate db ops
 def db_gen():
     c.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER, username TEXT, password TEXT)") #create user table
     c.execute("INSERT INTO users VALUES(0, '%s', '%s')" % ('admin', hashlib.sha256('password').hexdigest()))#stick base user into the db
+    c.execute("CREATE TABLE IF NOT EXISTS sessions(hash_id TEXT, id INTEGER, o_dist INTEGER, o_height INTEGER, focus INTEGER")#create session table
 
-db_gen()
+#db_gen()
 #==========================================================
 
 db.commit() #save changes
--- softdev2_FinalProject/utils/db_func.py
+++ softdev2_FinalProject/utils/db_func.py
@@ -1,6 +1,9 @@
-import sqlite3, hashlib, os
+import sqlite3, hashlib, os, zlib, json
 
-f=os.path.abspath(os.path.dirname(__file__)) + "/../data/lens.db"
+f = os.path.abspath(os.path.dirname(__file__)) + "/../data/lens.db"
+#f = "../data/lens.db"
+
+#alphanumeric = "1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm,./;'[]\`!@#$%^&*()_+-=|}{:?><"
 
 # Encrypt password - Returns SHA256
 def encrypt_password(password):
@@ -11,33 +14,110 @@ def encrypt_password(password):
 def login(username, password):
     db = sqlite3.connect(f)
     c = db.cursor()
+    st = False
     c.execute("SELECT username, password FROM users WHERE username = '%s'" % (username))
     for account in c:
         u = account[0]
         p = account[1]
         # Check if usernames and encrypted passwords match
         if username == u and encrypt_password(password) == p:
-            return True
-    return False
+            st = True
+    db.commit()
+    db.close()
+    return st
 
 # Create account - Returns true if successful, false otherwise
 def create_account(username, password):
     db = sqlite3.connect(f)
     c = db.cursor()
+    st = False
     if not does_username_exist(username):
         # Add user to accounts table
         c.execute("SELECT * FROM users ORDER BY id DESC LIMIT 1")
         user_id = int(c.fetchone()[0]) + 1
         c.execute("INSERT INTO users VALUES(%d, '%s', '%s')" % (user_id, username, encrypt_password(password)))
-        db.commit()
-        db.close()
-        return True
-    return False
+        st = True
+    db.commit()
+    db.close()    
+    return st
 
 def does_username_exist(username):
     db = sqlite3.connect(f)
     c = db.cursor()
     c.execute("SELECT username FROM users WHERE username = '%s'" % (username))
+    st = False
     for account in c:
-        return True
-    return False
+        st = True
+    db.commit()
+    db.close()
+    return st
+
+def change_password(username, old_password, new_password):
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    st = False
+    if login(username, old_password):
+        c.execute("UPDATE users SET password='%s' WHERE username = '%s' AND password = '%s'" % (encrypt(new_password), username, old_password))
+        st = True
+    db.commit()
+    db.close()
+    return st
+
+def does_hash_exist(hashcode):
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    #checks if hash already in use
+    st = c.execute("SELECT 1 FROM sessions WHERE hash_id='%s'" % (hashcode)).fetchall() != []
+    db.commit()
+    db.close()
+    return st
+
+def create_new_hash():
+    new_hash = zlib.adler32(os.urandom(10)).hexdigest()
+    while(does_hash_exist(new_hash)):
+        new_hash = zlib.adler32(os.urandom(10)).hexdigest()
+    return new_hash
+
+def create_session(username, o_dist, o_height, focus):
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    #assumes user exists
+    c.execute("INSERT INTO sessions VALUES('%s', (SELECT id FROM users WHERE username = '%s'), %d, %d, %d" % (create_new_hash(), username, o_dist, o_height, focus))
+    db.commit()
+    db.close()
+
+def get_session(hashcode):
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    info = {}
+    raw = c.execute("SELECT * FROM sessions WHERE hash_id = '%s'" % hashcode).fetchall()[0]
+    info["hash"] = raw[0]
+    info["id"] = raw[1]
+    info["o_dis"] = raw[2]
+    info["o_height"] = raw[3]
+    info["focus"] = raw[4]
+    db.commit()
+    db.close()
+    return info
+
+def get_owned_sessions(id):
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    info = [hash_id[0] for hash_id in c.execute("SELECT hash_id FROM sessions WHERE id = %d" % (id)).fetchall()]
+    db.commit()
+    db.close()
+    return info
+
+def update_session(hashcode, o_dist, o_height, focus):
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    c.execute("UPDATE sessions SET o_dist='%s', o_height=%d, focus=%d  WHERE hash_id = '%s'" % (o_dist, o_height, focus, hashcode))
+    db.commit()
+    db.close()
+
+'''
+print(get_owned_sessions(0))
+print(get_session("apple"))
+update_session("apple", 10, 14, 14)
+print(get_session("apple"))
+'''
