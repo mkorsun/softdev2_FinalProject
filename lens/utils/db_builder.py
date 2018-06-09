61b3e72d71a1bcbae2b46135f304d100c1896fd0
root <mkorsun@stuy.edu>
2018-06-09 04:28:06 +0000

    change of permissions

--- lens/utils/db_builder.py
+++ lens/utils/db_builder.py
@@ -3,6 +3,12 @@ import sqlite3, hashlib, os   #enable control of an sqlite database
 basedir = os.path.abspath(os.path.dirname(__file__))
 global f
 f=basedir+"/../data/lens.db"
+#exist = os.path.exists(f)
+#make = True
+
+#if (exist):
+#	make = False
+
 db=sqlite3.connect(f) #creates db if doesnt exist, and connect
 c=db.cursor() #facilitates b ops
 
@@ -13,7 +19,9 @@ def db_gen():
     c.execute("INSERT INTO users VALUES(0, '%s', '%s')" % ('admin', hashlib.sha256('password').hexdigest()))#stick base user into the db
     c.execute("CREATE TABLE IF NOT EXISTS sessions(hash_id TEXT, id INTEGER, o_dist INTEGER, o_height INTEGER, focus INTEGER, sign INTEGER)")#create session table
 
+#if (make):
 db_gen()
+
 #==========================================================
 
 db.commit() #save changes
